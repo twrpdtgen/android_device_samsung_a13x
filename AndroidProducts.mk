c5f7fd7de97992466cf0fcef46ca37339b58942a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-07 20:36:37 +0100

    a13x: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: samsung Model: SM-A136B Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a13x.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a13x-user \
+    omni_a13x-userdebug \
+    omni_a13x-eng
